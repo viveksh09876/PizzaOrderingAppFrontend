fb5c06939c0faa5ea5ab53676704cb269fa4aa4c
narendra.singh <narendra.singh@sirionlabs.com>
2017-07-10 01:44:59 +0530

    envirnment file url update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  cmsApiPath: 'https://mavin360.com/demo/nkd/dev',
-  posApiPath: 'http://35.185.240.172/nkd/index.php'
+  cmsApiPath: 'http://nkdpizza.com/beta/dev',
+  posApiPath: 'http://nkdpizza.com/beta/pos/index.php'
 };
