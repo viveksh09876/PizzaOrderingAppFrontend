44181363337a236a3dace9b4acdcc1b5e736333b
Vivek <globusweblogix.india@gmail.com>
2017-07-15 17:57:15 +0530

    sorting of modifiers

---
+++ src/app/modsort.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ModsortPipe } from './modsort.pipe';
+
+describe('ModsortPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ModsortPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -7,46 +7,24 @@ export class SortPipe implements PipeTransform {
 
   transform(array: any[], field: string): any[] {
     array.sort((a: any, b: any) => {
-
-      let opt_a = 0; 
-      let opt_b = 0;
-
-      // if(a.Option.is_checked) {
-      //   opt_a = 1;
-      // }
-
-      // if(b.Option.is_checked) {
-      //   opt_b= 1;
-      // } 
-
-      //alphabetical
-     // if(opt_a == 0 && opt_b == 0) {
-       //console.log(a, b)
-        if(a.Option.name < b.Option.name) return -1;
-        if(a.Option.name > b.Option.name) return 1;
-        return 0;
-      //}
-      //console.log(a,b);
-      // let opt_a = 0; 
-      // let opt_b = 0;
-
-      // if(a.Option.is_checked) {
       
-      // }
-      // if(a.Option.is_included_mod) {
-      //   opt_a = 1;
-      // }else{
-      //   opt_a = 0;
-      // }
+      if(a.Option.is_checked || b.Option.is_checked) {
 
-      // if(b.Option.is_included_mod) {
-      //   opt_b = 1;
-      // }else{
-      //   opt_b = 0;
-      // }
+        if(a.Option.is_checked && b.Option.is_checked) {
+          return 0;
+        }else if(a.Option.is_checked && !b.Option.is_checked) {
+          return -1;
+        }else if(!a.Option.is_checked && b.Option.is_checked) {
+          return 1;
+        }
 
-//      return (a === b)? 0 : a? -1 : 1;
+      }else{
 
+        var textA = a.Option.name.toUpperCase();
+        var textB = b.Option.name.toUpperCase();
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+
+       }
     });
     return array;
   }
