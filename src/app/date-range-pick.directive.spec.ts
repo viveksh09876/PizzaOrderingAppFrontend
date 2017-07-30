f8be4d702cf7c97dafcddca0001890632b58ab40
Vivek <globusweblogix.india@gmail.com>
2017-07-30 17:17:42 +0530

    date files added

---
+++ src/app/date-range-pick.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DateRangePickDirective } from './date-range-pick.directive';
+
+describe('DateRangePickDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DateRangePickDirective();
+    expect(directive).toBeTruthy();
+  });
+});
